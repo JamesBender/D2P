6a31a45bf2cb630dca4f6bbd43ad479d4242a6ba
James Bender <James@JamesCBender.com>
2021-05-18 10:11:54 -0700

    crossover notes and JSX Ripout

---
+++ Crossover/Discovery/TekP-2020-12-10-0009/stuff.sql
@@ -0,0 +1,19 @@
+select CASE WHEN A.BdmCobraReason IN ('200','201','202','204','205','206','207','208','209','210','LEVNT1','LEVNT2','LEVNT3','LEVNT4','LEVNT5') THEN
+                                CASE WHEN A.BdmRelationship = 'SPS' AND (SELECT COUNT(*) FROM dbo.U_dsi_BDM_EDISCOBQB2 B WITH (NOLOCK) WHERE A.BdmEEID = B.BdmEEID AND B.BdmDedCode IN ('XOMFT','XOMPT') AND B.BdmRelationship IN ('CHL','DPC','STC','DCH')) = 1 THEN 'EE+CHILD'
+                                    WHEN A.BdmRelationship = 'SPS' AND (SELECT COUNT(*) FROM dbo.U_dsi_BDM_EDISCOBQB2 B WITH (NOLOCK) WHERE A.BdmEEID = B.BdmEEID AND B.BdmDedCode IN ('XOMFT','XOMPT') AND B.BdmRelationship IN ('CHL','DPC','STC','DCH')) > 1 THEN 'EE+CHILDREN'
+                                    WHEN A.BdmRelationship IN ('CHL','DPC','STC','DCH') THEN 'EE+CHILDREN'
+                                    ELSE
+										CASE WHEN (SELECT COUNT(*) FROM dbo.U_dsi_BDM_EDISCOBQB2 B WITH (NOLOCK) WHERE A.BdmEEID = B.BdmEEID AND B.BdmDedCode IN ('XOMFT','XOMPT') AND B.BdmRelationship IN ('CHL','DPC','STC','DCH')) = 1 THEN 'EE+CHILD'
+											WHEN (SELECT COUNT(*) FROM dbo.U_dsi_BDM_EDISCOBQB2 B WITH (NOLOCK) WHERE A.BdmEEID = B.BdmEEID AND B.BdmDedCode IN ('XOMFT','XOMPT') AND B.BdmRelationship IN ('CHL','DPC','STC','DCH')) > 1 THEN 'EE+CHILDREN'
+											ELSE 'EE'
+										END
+                                END
+                            END, *
+FROM dbo.U_dsi_BDM_EDISCOBQB2 A WITH (NOLOCK)
+    JOIN dbo.EmpPers WITH (NOLOCK)
+        ON EepEEID = A.BdmEEID
+    LEFT JOIN dbo.Contacts WITH (NOLOCK)
+        ON ConEEID = A.BdmEEID
+        AND ConSystemID = A.BdmDepRecID
+    WHERE BdmDedCode IN ('XOMFT','XOMPT')
+	AND BdmEEID = 'CXO93D000030'
