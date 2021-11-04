d205af4e5d002860b67145e16fa31fd84a90132a
James Bender <James@JamesCBender.com>
2021-11-04 08:18:25 +0000

    update

---
+++ LIPP/LIPPERT/Tek-2021-06-15-0001/Stuff 2.sql
@@ -0,0 +1,37 @@
+--select drvhrstatusDate, * from dbo.U_EAV3_drvTbl;
+
+--sp_getEEID 'Schinella' -- DIED8K000020
+--sp_getEEID 'Clark'
+--sp_getEEID 'Wathke' -- DUZT9V000020
+--sp_getEEID 'Benehoule' -- DPJTIK000020 -- NOT i EE
+--sp_getEEID 'Gajewsky' -- DGI8MS01K020
+--sp_getEEID 'Lopez' -- DB5C1C00O020
+--sp_getEEID 'Samaniego' -- DGI8XS015020
+
+
+SELECT audEEID AS hsdEEID, audKey2 AS HsdCOID, audFieldName AS hsdFieldName, CASE WHEN audFieldName = 'EecEmplStatus' THEN EecEmplStatusStartDate END
+FROM dbo.U_EAV3_StatusDate_Audit
+JOIN dbo.vw_int_EmpComp WITH (NOLOCK)
+        ON EecEEID = audEEID 
+        AND EecCoID = audKey2
+--WHERE audEEID = 'DIED8K000020'
+--WHERE audEEID = 'DJU48K000020'
+--WHERE audEEID = 'DUZT9V000020'
+--WHERE audEEID = 'DPJTIK000020'
+--WHERE audEEID = 'DGI8MS01K020'
+--WHERE audEEID = 'DB5C1C00O020'
+WHERE audEEID = 'DGI8XS015020'
+
+
+
+		select EjhJobEffDate, EjhReason, * from EmpHJob A
+		--where EjhEEID = 'DIED8K000020' -- 'DB5AKC000020'
+		--where EjhEEID = 'DPJTIK000020'
+		--where EjhEEID = 'DGI8MS01K020'
+		WHERE EjhEEID = 'DGI8XS015020'
+		order by A. EjhJobEffDate desc
+
+--SELECT EecEmplStatusStartDate, EecCOID, * From EmpComp where EecEEID = 'DPJTIK000020'
+
+select top 10 audAction, * from dbo.vw_AuditData A WITH (NOLOCK) where audKey1Value = 'DGI8XS015020' AND audFieldName IN ('EecEmplStatus','EecSalaryOrHourly','EecFullTimeOrPartTime','EecEEType') AND audTableName = 'EmpComp' order by A.audDateTime desc
+
